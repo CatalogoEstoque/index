d1075476cbbe59b8676752eb43d0e42ee007def0
flavioalves2589 <143047951+flavioalves2589@users.noreply.github.com>
2025-02-20 10:46:12 -0300

    scriptindex.js

--- scriptindex.js
+++ scriptindex.js
@@ -37,21 +37,99 @@ if (!localStorage.getItem("loggedIn")) {
             .catch(error => console.error("Erro ao carregar o arquivo:", error));
     });
 
-    function filterCategory(category) {
-        selectedCategory = category;
-     selectedFormat = "";
-        document.getElementById("active-filter").innerText = `Filtro ativo: ${category}`;
-        applyFilters();
+
+    function filterSector(sector) {
+    // Verifica se o filtro já está ativo
+    if (selectedSector === sector) {
+        // Se o filtro já estiver ativo, desfaça o filtro
+        selectedSector = "";
+        document.getElementById("active-filter").innerText = "Filtro ativo: Nenhum";
+    } else {
+        // Caso contrário, ative o filtro
+        selectedSector = sector;
+        selectedCategory = "";
+        selectedFormat = "";
+        document.getElementById("active-filter").innerText = `Filtro ativo: ${sector}`;
     }
+
+    // Remove a classe 'selected' de todos os botões de setor
+    document.querySelectorAll(".category").forEach(button => {
+        button.classList.remove("selected");
+    });
+
+    // Adiciona a classe 'selected' ao botão clicado
+    document.querySelectorAll(".category").forEach(button => {
+        if (button.innerText === selectedSector) {
+            button.classList.add("selected");
+        }
+    });
+
+    applyFilters();
+}
+
+
+    function filterCategory(category) {
+    // Verifica se o filtro já está ativo
+    if (selectedCategory === category) {
+        // Se o filtro já estiver ativo, desfaça o filtro
+        selectedCategory = "";
+        document.getElementById("active-filter").innerText = "Filtro ativo: Nenhum";
+    } else {
+        // Caso contrário, ative o filtro
+        selectedSector = "";
+        selectedFormat = "";
+        selectedCategory = category;
+        document.getElementById("active-filter").innerText = `Filtro ativo: ${category}`;
+    }
+
+    // Remove a classe 'selected' de todos os botões de categoria
+    document.querySelectorAll(".category").forEach(button => {
+        button.classList.remove("selected");
+    });
+
+    // Adiciona a classe 'selected' ao botão clicado
+    document.querySelectorAll(".category").forEach(button => {
+        if (button.innerText === selectedCategory) {
+            button.classList.add("selected");
+        }
+    });
+
+    applyFilters();
+}
+
 
     function filterFormat(format) {
-        selectedFormat = format;
+    // Verifica se o filtro já está ativo
+    if (selectedFormat === format) {
+        // Se o filtro já estiver ativo, desfaça o filtro
+        selectedFormat = "";
+        document.getElementById("active-filter").innerText = "Filtro ativo: Nenhum";
+    } else {
+        // Caso contrário, ative o filtro
+        selectedSector = "";
         selectedCategory = "";
+        selectedFormat = format;
         document.getElementById("active-filter").innerText = `Filtro ativo: ${selectedCategory} ${selectedFormat ? '  Formato: ' + selectedFormat : ''}`;
-        applyFilters();
     }
 
+    // Remove a classe 'selected' de todos os botões de formato
+    document.querySelectorAll(".category").forEach(button => {
+        button.classList.remove("selected");
+    });
+
+    // Adiciona a classe 'selected' ao botão clicado
+    document.querySelectorAll(".category").forEach(button => {
+        if (button.innerText === selectedFormat) {
+            button.classList.add("selected");
+        }
+    });
+
+    applyFilters();
+}
+
+
     function clearCategory() {
+        selectedSector = "";
         selectedCategory = "";
         selectedFormat = "";
         applyFilters();
@@ -59,33 +137,41 @@ if (!localStorage.getItem("loggedIn")) {
         
     }
 
-    function applyFilters() {
-        const codeFilter = document.getElementById("filterCode").value.toLowerCase();
-        const descriptionFilter = document.getElementById("filterDescription").value.toLowerCase();
-        const locationFilter = document.getElementById("filterLocation").value.toLowerCase();
+   function applyFilters() {
+    const codeFilter = document.getElementById("filterCode").value.toLowerCase();
+    const descriptionFilter = document.getElementById("filterDescription").value.toLowerCase();
+    const locationFilter = document.getElementById("filterLocation").value.toLowerCase();
+    const equipamentoFilter = document.getElementById("filterEquipamento").value.toLowerCase();
 
-        let filteredProducts = allProducts;
+    let filteredProducts = allProducts;
 
-        if (selectedCategory) {
-            filteredProducts = filteredProducts.filter(product => product["Categoria"] === selectedCategory);
-        }
-        if (selectedFormat) {
-            filteredProducts = filteredProducts.filter(product => product["Formato"] && product["Formato"].toLowerCase() === selectedFormat.toLowerCase());
-        }
-        if (codeFilter) {
-            filteredProducts = filteredProducts.filter(product => String(product.Material).toLowerCase().includes(codeFilter));
-        }
-        if (descriptionFilter) {
-            filteredProducts = filteredProducts.filter(product => String(product.TxtBreveMaterial).toLowerCase().includes(descriptionFilter));
-        }
-        if (locationFilter) {
-            filteredProducts = filteredProducts.filter(product => String(product["Pos.dpst."]).toLowerCase().includes(locationFilter));
-        }
-
-        document.getElementById("filtered-count").innerText = `Total Filtro: ${filteredProducts.length}`;
-
-        renderProducts(filteredProducts);
+    if (selectedCategory) {
+        filteredProducts = filteredProducts.filter(product => product["Categoria"] === selectedCategory);
     }
+    if (selectedFormat) {
+        filteredProducts = filteredProducts.filter(product => product["Formato"] && product["Formato"].toLowerCase() === selectedFormat.toLowerCase());
+    }
+    if (selectedSector) {
+        filteredProducts = filteredProducts.filter(product => product["Setor"] && product["Setor"].toLowerCase() === selectedSector.toLowerCase());
+    }
+    if (codeFilter) {
+        filteredProducts = filteredProducts.filter(product => String(product.Material).toLowerCase().includes(codeFilter));
+    }
+    if (descriptionFilter) {
+        filteredProducts = filteredProducts.filter(product => String(product.TxtBreveMaterial).toLowerCase().includes(descriptionFilter));
+    }
+    if (locationFilter) {
+        filteredProducts = filteredProducts.filter(product => String(product["Pos.dpst."]).toLowerCase().includes(locationFilter));
+    }
+    if (equipamentoFilter) {
+        filteredProducts = filteredProducts.filter(product => String(product.Equipamento).toLowerCase().includes(equipamentoFilter));
+    }
+
+    document.getElementById("filtered-count").innerText = `Total Filtro: ${filteredProducts.length}`;
+
+    renderProducts(filteredProducts);
+}
+
 
     function renderProducts(products) {
         const productList = document.getElementById("product-list");
@@ -104,9 +190,10 @@ if (!localStorage.getItem("loggedIn")) {
         <h2>${product.Material ?? 'Material não especificado'}</h2>
         <div class="product-details">
             <p>${product.TxtBreveMaterial ?? 'Descrição não disponível'}</p>
-            <p><strong>Unidade:</strong> ${product.UMB ?? 'N/A'}</p>
-            <p><strong>Posição:</strong> ${product['Pos.dpst.'] ?? 'Localização não informada'}</p>
-        </div>
+            <span><strong>Unidade:</strong> ${product.UMB ?? 'N/A'}</span> &nbsp; | &nbsp;
+            <span><strong>Posição:</strong> ${product['Pos.dpst.'] ?? 'Localização não informada'}</span>
+            <p>${product.Equipamento ?? 'Equipamento não cadastrado'}</p>
+       </div>
             `;
             productList.appendChild(div);
         });
